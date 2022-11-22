e9a2b979ca9d3415fc3e8bd62a5989c9550fda19
CHENHANG WEN <conan@CHENHANGdeMacBook-Air.local>
2022-11-22 10:25:28 +0900

    use dotenv to manage PORT

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { User } from "./models/User";
 import { BearerStrategy } from "passport-azure-ad";
 import { createBearerStrategy } from "./middlewares/createBearerStrategy";
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 const routes = [UserController];
 
