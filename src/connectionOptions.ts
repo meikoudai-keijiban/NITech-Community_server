6c41dd0aa67b1685257884ec9354d12c5e2bdeb7
CHENHANG WEN <conan@CHENHANGdeMacBook-Air.local>
2023-03-19 20:34:31 +0900

    turn migdation to false, only synchronize true

--- src/connectionOptions.ts
+++ src/connectionOptions.ts
@@ -5,6 +5,8 @@ import { User } from "./models/User";
 import { Posting } from "./models/Posting";
 import { Comment } from "./models/Comment";
 
+import { nicDatabaseMigration1679224123083 } from "./migrations/1679224123083-nicDatabaseMigration";
+
 const databaseConnectionOption: ConnectionOptions = {
     type: "mariadb",
     name: "nicDatabase",
@@ -14,8 +16,9 @@ const databaseConnectionOption: ConnectionOptions = {
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    migrations: ["src/migrations/*.ts"],
-    migrationsRun: true,
+    // migrations: ["src/migrations/*.ts"],
+    migrations: [nicDatabaseMigration1679224123083],
+    // migrationsRun: true,
 }
 
 const connectionOptions: ConnectionOptions[] = [
