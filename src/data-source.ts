ac5f6cac64d7623eee647bca7a198c81bb77aa3e
CHENHANG WEN <conan@CHENHANGdeMacBook-Air.local>
2023-03-19 22:09:21 +0900

    add data-sourcr.ts for generating migration files

---
+++ src/data-source.ts
@@ -0,0 +1,32 @@
+// migration 自動作成に使用
+// 詳細は、https://qiita.com/Aurum64/items/f5962bd2a643447dbef9
+// npx typeorm-ts-node-commonjs migration:generate src/migrations/nicDatabaseMigration -d src/data-source.ts
+// npx typeorm-ts-node-commonjs migration:run -d src/data-source.ts
+
+
+// npx typeorm-ts-node-commonjs migration:generate dist/migrations/nicDatabaseMigration -d dist/data-source.js
+// npx typeorm-ts-node-commonjs migration:run -d dist/data-source.js
+
+
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "./models/User"
+import { Posting } from "./models/Posting"
+import { Comment } from "./models/Comment"
+
+export const AppDataSource = new DataSource({
+    type: "mariadb",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "root",
+    database: "nicDatabase",
+    synchronize: true,
+    logging: false,
+    entities: [User, Posting, Comment],
+    migrations: ['dist/migrations/*.js'],
+    // migrations: ['src/migrations/*.ts'],
+    // cli: {
+    //     "migrationsDir": "migration"
+    // }
+})
