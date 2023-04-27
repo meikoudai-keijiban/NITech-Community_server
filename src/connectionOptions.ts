ac63cb9f4bb38ab60fabbf21a04ff47c40048533
CHENHANG WEN <conan@CHENHANGdeMacBook-Air.local>
2023-04-27 17:04:03 +0900

    fix the file name

--- src/connectionOptions.ts
+++ src/connectionOptions.ts
@@ -5,7 +5,7 @@ import { User } from "./models/User";
 import { Posting } from "./models/Posting";
 import { Comment } from "./models/Comment";
 
-import { nicDatabaseMigration1679224123083 } from "./migrations/1679224123083-nicDatabaseMigration";
+import { nicDatabaseMigration1680241228245 } from "./migrations/1680241228245-nicDatabaseMigration";
 
 const databaseConnectionOption: ConnectionOptions = {
     type: "mariadb",
@@ -17,7 +17,7 @@ const databaseConnectionOption: ConnectionOptions = {
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     // migrations: ["src/migrations/*.ts"],
-    migrations: [nicDatabaseMigration1679224123083],
+    migrations: [nicDatabaseMigration1680241228245],
     migrationsRun: true,
 }
 
