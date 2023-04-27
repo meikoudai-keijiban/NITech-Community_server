0ad306c1f6d1baff59c7ec0cb8934591536c4397
CHENHANG WEN <conan@CHENHANGdeMacBook-Air.local>
2023-04-28 04:45:40 +0900

    get the latest posting, if thers is not maxid

--- src/controllers/PostingController.ts
+++ src/controllers/PostingController.ts
@@ -9,60 +9,64 @@ import { User } from "../models/User";
 
 @JsonController()
 export class PostingController {
-    private readonly postingService: PostingService;
+  private readonly postingService: PostingService;
 
-    constructor() {
-      this.postingService = new PostingService();
+  constructor() {
+    this.postingService = new PostingService();
+  }
+
+  @Get("/postings")
+  @UseBefore(passport.authenticate("oauth-bearer", { session: false }))
+  public async getPostingByMaxId(
+    @QueryParam("max_id") max_id: number = Infinity,
+    @QueryParam("n") n: number = 5,
+  ): Promise<Posting[] | null> {
+    if (max_id) {
+      return this.postingService.findPostingsBeforeMaxId(n, max_id);
+    }else {
+      return this.postingService.findPostingsLatest(n)
+    }
+  }
+
+  @Get("/postings/:postingId")
+  @UseBefore(passport.authenticate("oauth-bearer", { session: false }))
+  public async getPostingDetail(
+    @Param("postingId") postingId: number
+  ): Promise<Posting | null> {
+    return this.postingService.findOnePostingById(postingId);
+  }
+
+  @Get("/postings/all")
+  @UseBefore(passport.authenticate("oauth-bearer", { session: false }))
+  public async getAllPostings(): Promise<Posting[]> {
+    return this.postingService.findAllPostings();
+  }
+
+  @Get("/postings/part")
+  @UseBefore(passport.authenticate("oauth-bearer", { session: false }))
+  public async getPartPostings(
+    @QueryParam("skip") skip: number = 0,
+    @QueryParam('take') take: number = 10,
+  ): Promise<Posting[]> {
+    return this.postingService.findPartPostings(skip, take);
+  }
+
+  @HttpCode(201)
+  @Post("/postings")
+  @UseBefore(passport.authenticate("oauth-bearer", { session: false }))
+  public postPosting(
+    @CurrentUser({ required: true }) user: User,
+    @Body() rawPosting: RawPosting,
+  ): Promise<Posting> {
+
+    const posting: Posting = {
+      title: rawPosting.title,
+      author: user,
+      postDate: new Date(),
+      content: rawPosting.content,
     }
 
-    @Get("/postings")
-    @UseBefore(passport.authenticate("oauth-bearer", {session: false}))
-    public async getPostingByMaxId(
-      @QueryParam("max_id") max_id: number = Infinity,
-      @QueryParam("n") n: number = 5,
-    ): Promise<Posting[] | null> {
-        return this.postingService.findPostingsBeforeMaxId(n, max_id);
-    }
+    return this.postingService.savePosting(posting);
+  }
 
-    @Get("/postings/:postingId")
-    @UseBefore(passport.authenticate("oauth-bearer", {session: false}))
-    public async getPostingDetail(
-      @Param("postingId") postingId: number
-    ): Promise<Posting | null> {
-        return this.postingService.findOnePostingById(postingId);
-    }
-
-    @Get("/postings/all")
-    @UseBefore(passport.authenticate("oauth-bearer", {session: false}))
-    public async getAllPostings(): Promise<Posting[]> {
-        return this.postingService.findAllPostings();
-    }
-
-    @Get("/postings/part")
-    @UseBefore(passport.authenticate("oauth-bearer", {session: false}))
-    public async getPartPostings(
-        @QueryParam("skip") skip: number = 0,
-        @QueryParam('take') take: number = 10,
-    ): Promise<Posting[]> {
-        return this.postingService.findPartPostings(skip, take);
-    }
-
-    @HttpCode(201)
-    @Post("/postings")
-    @UseBefore(passport.authenticate("oauth-bearer", { session: false }))
-    public postPosting(
-      @CurrentUser({ required: true }) user: User,
-      @Body() rawPosting: RawPosting,
-    ): Promise<Posting> {
-
-      const posting: Posting = {
-        title: rawPosting.title,
-        author: user,
-        postDate: new Date(),
-        content: rawPosting.content,
-      }
-  
-      return this.postingService.savePosting(posting);
-    }
-    
 }
