834ecc7196398d5054d957cedefb17c7b3489470
Conan-Wen <w245646037@gmail.com>
2023-09-11 10:34:02 +0900

    intMAXクラス変数を使用，Get(/postings)を簡潔に

--- src/controllers/PostingController.ts
+++ src/controllers/PostingController.ts
@@ -10,22 +10,19 @@ import { PostingService } from "../services/PostingService";
 @JsonController()
 export class PostingController {
   private readonly postingService: PostingService;
+  private readonly intMAX: number;
 
   constructor() {
     this.postingService = new PostingService();
+    this.intMAX = 2147483647; // データベースのINT型の最大値
   }
 
   @Get("/postings")
   @UseBefore(passport.authenticate("oauth-bearer", { session: false })) // eslint-disable-line @typescript-eslint/no-unsafe-argument
   public async getPostingByMaxId(
-    @QueryParam("max_id") max_id: number | undefined,
+    @QueryParam("max_id") max_id: number = this.intMAX,
     @QueryParam("n") n = 5,
   ): Promise<Posting[]> {
-    if (max_id === undefined) {
-      const IntMAX: number = 2147483647;
-      max_id = IntMAX;
-    }
-
     return this.postingService.findPostingsBeforeMaxId(n, max_id);
   }
 
